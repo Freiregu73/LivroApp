4a1b7a73559e1c4131fc66fcfc0930225204397d
Freiregu73 <gustavofreire191@gmail.com>
2026-05-28 21:37:42 -0300

    projeto git

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -7,8 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<Livros> lstLivros;
+    RecyclerView IDRecLivros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        IDRecLivros = findViewById(R.id.IDRecLivros);
+        AdapterLivros adapterLivros = new AdapterLivros(getApplicationContext(),lstLivros);
+
 
     }
 }
